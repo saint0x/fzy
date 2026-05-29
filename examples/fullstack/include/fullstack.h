369717c07aaa5477797013033712801b4540cb41
saint <saint.0x.saint@gmail.com>
2026-05-29 12:41:52 -0400

    Refresh production gate baselines

--- examples/fullstack/include/fullstack.h
+++ examples/fullstack/include/fullstack.h
@@ -14,15 +14,16 @@ typedef int32_t (*fz_callback_i32_v0)(int32_t arg);
 int32_t fz_host_init(void);
 int32_t fz_host_shutdown(void);
 int32_t fz_host_cleanup(void);
+int32_t fz_host_last_error_code(void);
+int32_t fz_host_last_error_class(void);
+const char* fz_host_last_error_message(void);
 int32_t fz_host_register_callback_i32(int32_t slot, fz_callback_i32_v0 cb);
 int32_t fz_host_invoke_callback_i32(int32_t slot, int32_t arg);
 
-typedef struct model.types.UserRow {
-} model.types.UserRow;
+typedef struct model_types_UserRow {
+} model_types_UserRow;
 
-int32_t fs_open(const uint8_t* path_borrowed, size_t len);
-int32_t fs_write(int32_t fd, const uint8_t* ptr_borrowed, size_t len);
-uint32_t hash32(const uint8_t* ptr_borrowed, size_t len);
+/* no exported extern "C" functions found */
 
 #ifdef __cplusplus
 }
--- examples/live_server/include/live_server.h
+++ examples/live_server/include/live_server.h
@@ -14,17 +14,19 @@ typedef int32_t (*fz_callback_i32_v0)(int32_t arg);
 int32_t fz_host_init(void);
 int32_t fz_host_shutdown(void);
 int32_t fz_host_cleanup(void);
+int32_t fz_host_last_error_code(void);
+int32_t fz_host_last_error_class(void);
+const char* fz_host_last_error_message(void);
 int32_t fz_host_register_callback_i32(int32_t slot, fz_callback_i32_v0 cb);
 int32_t fz_host_invoke_callback_i32(int32_t slot, int32_t arg);
 
-typedef struct UserRow {
-} UserRow;
+typedef struct model_types_UserRow {
+} model_types_UserRow;
 
-typedef enum HttpState {
-} HttpState;
+typedef enum model_types_HttpState {
+} model_types_HttpState;
 
-uint32_t server_hash32(const uint8_t* ptr_borrowed, size_t len);
-int32_t server_write(int32_t fd, const uint8_t* ptr_borrowed, size_t len);
+/* no exported extern "C" functions found */
 
 #ifdef __cplusplus
 }
